85a2f1437b695aaef9eea197e490b82f819d1846
Antônio da Silva <antoniocarlos.dev@gmail.com>
2017-04-03 20:03:58 +0800

    Initial Commit

---
+++ Youtube Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube Player
+//
+//  Created by Antonio da Silva on 03/04/2017.
+//  Copyright © 2017 Antonio da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
